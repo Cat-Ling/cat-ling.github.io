7bde14673f0928fd5a3680618ea61eeb8edbed2e
Catie <130665030+Cat-Ling@users.noreply.github.com>
2024-12-03 08:36:46 +0530

    Update DuckDuckGo.js
    added comment fixes for better understanding what's going on, for me.

--- DuckDuckGo.js
+++ DuckDuckGo.js
@@ -1,14 +1,14 @@
 /*
- Malicious extension developers stay away please. I'll come hunt you
- down if I find you using this script.
+ Weird extension makers, stay away please. I'll come hunt you
+ down if I find you using this script. https://tinyurl.com/34eusjmt
  This script handles the submission of search queries and sends them
- to DuckDuckGo while sending some additional parameters to ensure
- privacy and functionality.
+ to DuckDuckGo while including some additional parameters whcih improve
+ functionality.
 */
 function submitDuckDuckGoSearch(query) {
     const form = document.createElement('form');
     form.action = 'https://duckduckgo.com/';
-    form.method = 'POST'; // Changed method to POST to pervent search tracking by URL. Note that this won't magically make you anonymous.
+    form.method = 'POST'; // Set the submit method to POST to prevent your searches from being visible in the URL and tracked by your browser.
 
     const Query = document.createElement('input');
     Query.type = 'hidden';
@@ -18,7 +18,7 @@ function submitDuckDuckGoSearch(query) {
     const SafeSearch = document.createElement('input');
     SafeSearch.type = 'hidden';
     SafeSearch.name = 'kp';
-    SafeSearch.value = '-2'; // I've disabled SafeSearch by using -2. You can set it to -1 to enable moderate filter.
+    SafeSearch.value = '-2'; // Disables safesearch.
 
     const method = document.createElement('input');
     method.type = 'hidden';
@@ -28,27 +28,27 @@ function submitDuckDuckGoSearch(query) {
     const theme = document.createElement('input');
     theme.type = 'hidden';
     theme.name = 'kae';
-    theme.value = 'd'; // This is the color code of search results. Default is d=Dark
+    theme.value = 'd'; // Theme. Default is d=Dark
 
     const header = document.createElement('input');
     header.type = 'hidden';
     header.name = 'ko';
-    header.value = '1'; // Floating header while you scroll.
+    header.value = '1'; // Floating navbar.
 
     const advertisements = document.createElement('input');
     advertisements.type = 'hidden';
     advertisements.name = 'k1';
-    advertisements.value = '-1'; // Disabled advertisements.
+    advertisements.value = '-1'; // Tells DuckDuckGo to disable ads.
 
     const videoPrefs = document.createElement('input');
     videoPrefs.type = 'hidden';
     videoPrefs.name = 'k5';
-    videoPrefs.value = '-1'; // This will prompt you whether you want to play the video directly on DuckDuckGo or go to that video's page.
+    videoPrefs.value = '-1'; // This will prompt you whether you want to play any video fromr results directly on DuckDuckGo or go to that video's page.
 
     const https = document.createElement('input');
     https.type = 'hidden';
     https.name = 'kh';
-    https.value = '1'; // Tells DuckDuckGo that you prefer https. We're already doing that in our modern browsers, but this value makes sure of it, so why not. Besides, the client I originally made this page for won't load http at all.
+    https.value = '1'; // Tells DuckDuckGo that you prefer https. We're already doing that in our modern browsers, but this value makes sure of it, so why not.
 
     const hideRef = document.createElement('input');
     hideRef.type = 'hidden';
@@ -63,19 +63,19 @@ function submitDuckDuckGoSearch(query) {
     const region = document.createElement('input');
     region.type = 'hidden';
     region.name = 'kl';
-    region.value = 'us-en'; // Change the value to change default search region. Default is United States, English.
+    region.value = 'us-en'; // Default search region. Default is United States, English.
 
     form.appendChild(Query); // Search query. (The thing you type to search :p)
-    form.appendChild(SafeSearch); // If you're thinking of using this for your app, remove this to set safesearch to default.
+    form.appendChild(SafeSearch); // If you're thinking of using this script for your site, remove this to set safesearch to default.
     form.appendChild(theme); // Remove this to set theme to default.
-    form.appendChild(header); // Remove this if you want the header to not be persistently floating.
+    form.appendChild(header); // Remove this if you want the navbar to not be persistently floating.
     form.appendChild(advertisements); // Remove this if you want advertisements on DuckDuckGo.
-    form.appendChild(method); // Remove if you want your search queries to be tracked by your browser.
+    form.appendChild(method); // Remove if you want your search queries to be tracked by your browser. (Use GET instead of POST requests)
     form.appendChild(videoPrefs); // Always shows video Preferences.
     form.appendChild(https); // Tells DuckDuckGo to only use https.
-    form.appendChild(hideRef); // Hide referrers.
-    form.appendChild(autoLR); // Auto load results as you scroll.
-    form.appendChild(region); // Defines your search region.
+    form.appendChild(hideRef); // Hides referrers.
+    form.appendChild(autoLR); // Automatically loads results as you scroll instead of using the paging system.
+    form.appendChild(region); // Sets your search region.
 
     document.body.appendChild(form);
 
